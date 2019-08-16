4614c82c6defb8a4589eb3bfd513d236a3c8049b
Matt Sharp <msharp19@users.noreply.github.com>
2019-08-16 13:31:24 +0800

    MC | Removed Assert & replaced with Require

--- Deployable/MoralityStorage/MoralityContentStorage.sol
+++ Deployable/MoralityStorage/MoralityContentStorage.sol
@@ -7,25 +7,25 @@ library SafeMath {
     
   function mul(uint256 a, uint256 b) internal pure returns (uint256){
     uint256 c = a * b;
-    assert(a == 0 || c / a == b);
+    require(a == 0 || c / a == b);
     return c;
   }
 
   function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b > 0);
+    require(b > 0);
     uint256 c = a / b;
-    assert(a == b * c + a % b);
+    require(a == b * c + a % b);
     return c;
   }
 
   function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
+    require(b <= a);
     return a - b;
   }
 
   function add(uint256 a, uint256 b) internal pure returns (uint256) {
     uint256 c = a + b;
-    assert(c >= a);
+    require(c >= a);
     return c;
   }
   
